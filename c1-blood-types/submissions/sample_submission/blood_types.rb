cc5ada30b0d55a9fba82ad5e72f918803781bf14
Gus Ortiz <guspidey@gmail.com>
2010-09-02 13:33:35 -0500

    Added submissions folder

---
+++ c1-blood-types/submissions/sample_submission/blood_types.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'yaml'
+
+class BloodTypes
+  attr_reader :combinations
+
+  # Fills the blood combinations with the missing set of possible blood tyoes
+  #
+  # @return [Hash] complete blood types combinations.
+  def fill_combinations
+    # your code
+  end
+
+  # all the code you might need
+
+  private
+
+  # Initializes the object loading the source file into
+  #
+  # @param [String] source the url of the input file.
+  # @private
+  def init(source)
+    # your code
+  end
+
+end
+
