2afe5673e25dd2537ef846b776d92bea4589cd2f
Urzuae <urzuae@gmail.com>
2010-09-02 13:13:47 -0500

    Fill out blood test

--- c1-blood-types/blood_types_test.rb
+++ c1-blood-types/blood_types_test.rb
@@ -1,11 +1,11 @@
 require 'blood_types'
 
 right_answer = {
-  "combination1" => {"dad" => "A+", "mom" => "B-", "child" => "AB+"},
-  "combination2" => {"dad" => "O-", "mom" => "B-", "child" => "B-"},
-  "combination3" => {"dad" => "B+", "mom" => "B-", "child" => "O+"},
-  "combination4" => {"dad" => "B-", "mom" => "B+", "child" => ["B+","O+"]},
-  "combination5" => {"dad" => "AB+", "mom" => "IMPOSSIBLE", "child" => "A-"},
+  "combination1" => {"dad" => "A+", "mom" => "B-", "child" => ["A+","A-","AB+","AB-","B+","B-","O+","O-"]},
+  "combination2" => {"dad" => "O-", "mom" => "B-", "child" => ["B-","O-"]},
+  "combination3" => {"dad" => "B+", "mom" => ["A+","A-","B+","B-","O+","O-"], "child" => "O+"},
+  "combination4" => {"dad" => "B-", "mom" => "B+", "child" => ["B+","B-","O+","O-"]},
+  "combination5" => {"dad" => "AB+", "mom" => "IMPOSSIBLE", "child" => "O-"},
 }
 
 puts BloodTypes.new('blood_types.yml').fill_combinations == right_answer
