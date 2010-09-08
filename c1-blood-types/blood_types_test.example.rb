e831f9d9feef5339f4698e0d235ad8fcd93f5fc3
Urzuae <urzuae@gmail.com>
2010-09-08 12:24:35 -0500

    Fix test result

--- c1-blood-types/blood_types_test.example.rb
+++ c1-blood-types/blood_types_test.example.rb
@@ -2,10 +2,10 @@ require 'blood_types'
 
 right_answer = {
   "combination1" => {"dad" => "A+", "mom" => "B-", "child" => ["A+","A-","AB+","AB-","B+","B-","O+","O-"]},
-  "combination2" => {"dad" => "O-", "mom" => "B-", "child" => ["B-","O-"]},
+  "combination2" => {"dad" => "O-", "mom" => ["AB+","AB-","B+","B-"], "child" => "B-"},
   "combination3" => {"dad" => "B+", "mom" => ["A+","A-","B+","B-","O+","O-"], "child" => "O+"},
   "combination4" => {"dad" => "B-", "mom" => "B+", "child" => ["B+","B-","O+","O-"]},
-  "combination5" => {"dad" => "AB+", "mom" => "IMPOSSIBLE", "child" => "O-"},
+  "combination5" => {"dad" => "AB+", "mom" => ["A+","A-","AB+","AB-","B+","B-","O+","O-"], "child" => "A-"}
 }
 
 puts BloodTypes.new('blood_types.yml').fill_combinations == right_answer
